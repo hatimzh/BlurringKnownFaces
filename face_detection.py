7647cc88c24fdfb3a0bbb4a8f0e89e40a27b6968
Hatim ZAHIRI <96501113+hatimzh@users.noreply.github.com>
2023-07-04 02:20:57 +0100

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -26,7 +26,7 @@ face_encodings, face_names = get_face_encodings()
 print("waiting a few seconds...")
 video = cv2.VideoCapture(0) #open the cam, you can change the 0 to 1 if you have an external webcam
 
-#video = cv2.VideoCapture("../Test/your_test_GIF_or_Video")
+#video = cv2.VideoCapture("./Test/your_test_GIF_or_Video")
 #output = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'XVID')
 #, int(video.get(cv2.CAP_PROP_FPS))
 #, (int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -39,47 +39,51 @@ print("Cam opened succefully\n press Q to exit.")
 scl = 2
 
 while True:
-    _, image = video.read()
+    x, image = video.read()
 
-    # Making current frame smaller so program runs faster
-    resized_image = cv2.resize(image, (int(image.shape[1]/scl), int(image.shape[0]/scl)))
-
-    # Converting current frame to RGB, since that's what the face recognition module uses
-    rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
-
-    # Retrieving face location coordinates and unknown encodings
-    face_locations = fr.face_locations(rgb_image)
-    unknown_encodings = fr.face_encodings(rgb_image, face_locations)
-
-    # Iterating through each encoding, as well as the face's location
-    for face_encoding, face_location in zip(unknown_encodings, face_locations):
-        # Comparing known faces with unknown faces
-        result = fr.compare_faces(face_encodings, face_encoding, 0.5)#you can change this third parameter to get best results
-
-        # Getting correct name if a match was found
-        if True in result:
-            name = face_names[result.index(True)]
-
-            # Setting coordinates for face location
-            top, right, bottom, left = face_location
-
-            # Drawing rectangle around face
-            imgface = cv2.rectangle(image, (left*scl, top*scl), (right*scl, bottom*scl), (255, 255, 255), 2)
-            # Bluring detected faces
-            imgface[top*scl:bottom*scl,left*scl:right*scl]=cv2.medianBlur(imgface[top*scl:bottom*scl,left*scl:right*scl],35)
-            # Setting font, as well as displaying text of name
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(image, name, (left*scl, bottom*scl + 20), font, 0.8, (255, 255, 255), 1)
+    if x :
     
-
+        # Making current frame smaller so program runs faster
+        resized_image = cv2.resize(image, (int(image.shape[1]/scl), int(image.shape[0]/scl)))
     
-    cv2.imshow("Blurring detected faces", image)
+        # Converting current frame to RGB, since that's what the face recognition module uses
+        rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
     
-    #output.write(image)
-
-    # Waiting until the Q key pressed
-    key=cv2.waitKey(1)
-    if key==ord('q'):
+        # Retrieving face location coordinates and unknown encodings
+        face_locations = fr.face_locations(rgb_image)
+        unknown_encodings = fr.face_encodings(rgb_image, face_locations)
+    
+        # Iterating through each encoding, as well as the face's location
+        for face_encoding, face_location in zip(unknown_encodings, face_locations):
+            # Comparing known faces with unknown faces
+            result = fr.compare_faces(face_encodings, face_encoding, 0.5)#you can change this third parameter to get best results
+    
+            # Getting correct name if a match was found
+            if True in result:
+                name = face_names[result.index(True)]
+    
+                # Setting coordinates for face location
+                top, right, bottom, left = face_location
+    
+                # Drawing rectangle around face
+                imgface = cv2.rectangle(image, (left*scl, top*scl), (right*scl, bottom*scl), (255, 255, 255), 2)
+                # Bluring detected faces
+                imgface[top*scl:bottom*scl,left*scl:right*scl]=cv2.medianBlur(imgface[top*scl:bottom*scl,left*scl:right*scl],35)
+                # Setting font, as well as displaying text of name
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(image, name, (left*scl, bottom*scl + 20), font, 0.8, (255, 255, 255), 1)
+        
+    
+        
+        cv2.imshow("Blurring detected faces", image)
+        
+        #output.write(image)
+    
+        # Waiting until the Q key pressed
+        key=cv2.waitKey(1)
+        if key==ord('q'):
+            break
+    else :
         break
     
 video.release()
